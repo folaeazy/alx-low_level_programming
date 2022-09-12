cbe50a7001e773b8ead6fcfe54f45516947893f3
eazy-Nyc <folaisrael11@gmail.com>
2022-09-12 08:44:07 -0700

    print comb modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,6 @@
  * File: 9-print_comb.c
  * Auth: Brennan D Baraban
  */
-
 #include <stdio.h>
 
 /**
@@ -19,7 +18,6 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
